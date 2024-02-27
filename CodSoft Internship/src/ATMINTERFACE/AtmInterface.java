af8f449e65a3e21b2bf2e73ff29b564960dc67e8
Kirtidas Sanjay There <118301351+kirtidasthere@users.noreply.github.com>
2024-02-27 21:04:45 +0530

    Add files via upload

---
+++ CodSoft Internship/src/ATMINTERFACE/AtmInterface.java
@@ -0,0 +1,14 @@
+package ATMINTERFACE;
+
+public interface AtmInterface{
+
+    public void withdraw(double withdrawAmount);
+
+    public void deposit(double depositAmount);
+
+    public void transactionHistory();
+
+    public void transfer(double transferAmount);
+
+    public void viewBalance();
+}
